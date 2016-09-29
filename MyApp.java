cc8b624bd2f2f2f5a004d62f817aa94d3441715f
Tomi - <Tomi ->
2016-09-29 14:21:51 +0300

    koodi siistitty

--- MyApp.java
+++ MyApp.java
@@ -2,13 +2,19 @@ package myapp;
 // Author: Tomi Kenkkilä
 public class main { 
     public static void main(String[] args) {  
-     Product product = new Product("Edam", 3.3, 120);      System.out.println("Product value is " +  product.countValue());      product.printProduct();      } 
+     Product product = new Product("Edam", 3.3, 120);
+      System.out.println("Product value is " +  product.countValue());
+      product.printProduct(); 
+  } 
 } 
  
  class Product { 
- private String name;  private double price;  private int amount; //Amount in storage
+ private String name;
+  private double price;
+  private int amount; //Amount in storage
   public Product(String name, double price, int amount)
- {   this.name = name;  
+ { 
+  this.name = name;  
  this.price = price; 
   this.amount = amount;
   } 
